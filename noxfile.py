212c1df2e6179e5cb9b2d0f81cab131af5ce5897
object-Object <object@objectobject.ca>
2023-12-20 22:30:02 -0500

    Add a bunch of integration tests

--- noxfile.py
+++ noxfile.py
@@ -1,57 +1,104 @@
-from pathlib import Path
+import os
+import platform
 import shutil
 import stat
+from pathlib import Path
 from typing import Any
+
 import nox
+
+MAPPINGS_NAMES = [
+    "mojmap",
+    "yarn",
+]
 
 nox.options.reuse_existing_virtualenvs = True
 
 
+def parametrize_output_dir():
+    return nox.parametrize(
+        "output_dir",
+        [Path(".ctt") / name for name in MAPPINGS_NAMES],
+        ids=MAPPINGS_NAMES,
+    )
+
+
+# sessions
+
+
 @nox.session
-def book(session: nox.Session):
-    session.install("copier", "copier-template-tester")
+def ctt(session: nox.Session):
+    session.install("copier-template-tester")
 
-    root = Path(".ctt/defaults")
-    if root.is_dir():
-        session.log(f"Removing directory: {root}")
-        shutil.rmtree(root, onerror=on_rm_error)
+    ctt_dir = Path(".ctt")
+    if ctt_dir.is_dir():
+        session.log(f"Removing directory: {ctt_dir}")
+        shutil.rmtree(ctt_dir, onerror=on_rm_error)
 
-    session.run("ctt", silent=True)
+    session.run("ctt", silent=not is_ci())
 
-    with session.chdir(root):
-        session.run(
-            "git",
-            "init",
-            external=True,
-            silent=True,
-        )
 
-        session.run(
-            "git",
-            "commit",
-            "--allow-empty",
-            "-m",
-            "Initial commit",
-            external=True,
-            silent=True,
-        )
+@nox.session
+@parametrize_output_dir()
+def setup(session: nox.Session, output_dir: Path):
+    session.chdir(output_dir)
 
-        session.run(
-            "copier",
-            "copy",
-            "gh:hexdoc-dev/hexdoc-hexcasting-template",
-            ".",
-            "--answers-file",
-            ".hexdoc-template-inputs.yml",
-            "--skip",
-            ".gitignore",
-            "--defaults",
-            silent=True,
-        )
+    session.install("copier")
 
-        session.install(".[dev]")
+    silent = not is_ci()
 
-        session.run("hexdoc", "serve")
+    session.run(
+        "git",
+        "init",
+        external=True,
+        silent=silent,
+    )
+
+    session.run(
+        "git",
+        "commit",
+        "--allow-empty",
+        "-m",
+        "Initial commit",
+        external=True,
+        silent=silent,
+    )
+
+    session.run(
+        "copier",
+        "copy",
+        "gh:hexdoc-dev/hexdoc-hexcasting-template",
+        ".",
+        "--answers-file",
+        ".hexdoc-template-inputs.yml",
+        "--skip",
+        ".gitignore",
+        "--defaults",
+        silent=silent,
+    )
+
+
+@nox.session
+@parametrize_output_dir()
+def book(session: nox.Session, output_dir: Path):
+    session.chdir(output_dir)
+
+    session.install(".")
+    session.run("pip", "freeze")
+
+    session.run("hexdoc", "build")
+    session.run("hexdoc", "merge")
+
+
+@nox.session(python=False)
+@parametrize_output_dir()
+def gradle_build(session: nox.Session, output_dir: Path):
+    session.chdir(output_dir)
+
+    session.run(gradle(), "build")
+
+
+# helpers
 
 
 def on_rm_error(func: Any, path: str, exc_info: Any):
@@ -59,3 +106,15 @@ def on_rm_error(func: Any, path: str, exc_info: Any):
     path_ = Path(path)
     path_.chmod(stat.S_IWRITE)
     path_.unlink()
+
+
+def is_ci():
+    return os.getenv("CI") == "true"
+
+
+def gradle():
+    match platform.system():
+        case "Windows":
+            return ".\\gradlew.bat"
+        case _:
+            return "./gradlew"
